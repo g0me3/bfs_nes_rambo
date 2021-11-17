c48a948a7a505bacf8a8ce06a18b087df87b414d
g0me3 <cah4e3@gmail.com>
2021-11-18 00:03:38 +0200

    finally fixed jp pass screen

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo off
 
-start "" "D:\work\tools\TextPad\TextPad.exe" "ram.inc" "macroses.inc" "bank5.inc" "bank6.inc" "bank7.inc" "apu_player.inc" "scroller.inc" "script\script_us.inc" "script\font_us.inc" "script\script_jp.inc" "script\font_jp.inc"
+start "" "D:\work\tools\TextPad\TextPad.exe" "ram.inc" "macroses.inc" "bank5.inc" "bank6.inc" "bank7.inc" "apu_player.inc" "game_data.inc" "scroller.inc" "script\script_us.inc" "script\font_us.inc" "script\script_jp.inc" "script\font_jp.inc"
 
 pause 0
 
